4ac231c1dec8a4034ad70325f632ec9eb8dcda89
Jamil Bailony <jBailony@outlook.com>
2020-07-26 18:14:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.body.removeChild(document.querySelector("main#main"));
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.textContent = "Jamil is the champion";
